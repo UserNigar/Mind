948e0476ff453fb4d255c396b5ec1a9c0614b9fa
NigarHasanzada <nigarnh-ap216@code.edu.az>
2025-06-28 16:18:01 +0400

    end version of homepage

--- frontend/src/components/pages/Side/Side.jsx
+++ frontend/src/components/pages/Side/Side.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import "./ArticleListWithSidebar.scss";
 import ArticleList from "../articleList/ArticleList";
+import { Link } from "react-router";
 
 const ArticleListWithSidebar = () => {
   return (
     <div className="container-layout">
       <div className="right-sidebar">
-        <div className="profile-box">
+        {/* <div className="profile-box">
           <img
             src="/default-avatar.png"
             alt="user"
@@ -15,13 +16,15 @@ const ArticleListWithSidebar = () => {
           />
           <h3>İstifadəçi adı</h3>
           <p>Profil haqqında qısa info...</p>
-        </div>
+        </div> */}
         <div className="menu-box">
-          <h4>Menu</h4>
           <ul>
-            <li>🏠 Ana səhifə</li>
-            <li>➕ Yeni post</li>
-            <li>💬 Mesajlar</li>
+            <li><Link to={"/"}> Esas sehife</Link></li>
+            <li><Link to={"/chat"}>➕ Yeni post</Link></li>
+            <li><Link to={"/chat"}>💬 Mesajlar</Link></li>
+            <li><Link>⚙️ Ayarlar</Link></li>
+            <li>⚙️ Ayarlar</li>
+            <li>⚙️ Ayarlar</li>
             <li>⚙️ Ayarlar</li>
           </ul>
         </div>
